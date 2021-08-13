b315169dca071f7d55e121b1d9215821e04c276b
7-longvv <7-longvv@phenikaa.local>
2021-08-13 22:23:31 +0700

    them file kiem tra chan le

---
+++ kiem_tra_chan_le.py
@@ -0,0 +1,7 @@
+number = float(input("Enter the number: "))
+if number % 2 == 0:
+    print("This is Even")
+elif number % 2 == 1:
+    print("This is Odd") 
+else:
+    print("This is not Integer")

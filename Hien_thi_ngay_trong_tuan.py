c9812a33e8e23caf2e958f546b5a9174f76a06fd
7-longvv <7-longvv@phenikaa.local>
2021-08-13 22:35:31 +0700

    them file hien thi ngay

---
+++ Hien_thi_ngay_trong_tuan.py
@@ -0,0 +1,16 @@
+Date = int(input("Enter you date:  "))
+if Date == 1:
+    print('Monday')
+elif Date == 2:
+    print('Tuesday')
+elif Date == 3:
+    print('Wednesday')
+elif Date == 4:
+    print('Thursday')
+elif Date == 5:
+    print('Friday')
+elif Date == 6:
+    print('Saturday')
+elif Date == 7:
+    print('Sunday')
+else: print('Error, out of range!!!')
